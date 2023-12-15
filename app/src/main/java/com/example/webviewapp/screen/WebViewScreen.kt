f8b05a0a1c7350a782a0a02971c932457fb5a9d4
prathameshduolop <prathamesh@duolop.com>
2023-12-15 21:01:23 +0530

    webView app add progess bar

--- app/src/main/java/com/example/webviewapp/screen/WebViewScreen.kt
+++ app/src/main/java/com/example/webviewapp/screen/WebViewScreen.kt
@@ -1,5 +1,6 @@
 package com.example.webviewapp.screen
 
+import android.graphics.Bitmap
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.background
@@ -9,10 +10,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -20,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -30,10 +34,22 @@ fun WebViewScreen() {
     val context = LocalContext.current
     val url = remember { mutableStateOf("https://newsnowbharatvarsh.com/") }
 
+    val isLoading = remember { mutableStateOf(true) }
+
     val webView = remember {
         WebView(context).apply {
             settings.javaScriptEnabled = true
-            webViewClient = WebViewClient()
+            webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    isLoading.value = true
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    isLoading.value = false
+                }
+            }
             loadUrl(url.value)
         }
     }
@@ -46,6 +62,16 @@ fun WebViewScreen() {
             modifier = Modifier
                 .fillMaxSize()
         )
+        if (isLoading.value) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Color.Transparent, shape = CircleShape)
+                    .alpha(0.8f)
+                    .align(Alignment.Center),
+                color = Color.Black
+            )
+        }
     }
     Box(
         Modifier
